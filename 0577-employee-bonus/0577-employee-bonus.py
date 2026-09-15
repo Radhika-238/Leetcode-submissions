6d9f6efe12d9e8f5a0f44812499046598a7f94d4
Radhika Agarwal <23radhika.agarwal@gmail.com>
2026-09-15 23:07:49 +0530

    Time: 381 ms (14.11%), Space: 68.4 MB (75.32%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    
+    df = employee.merge(bonus, left_on = 'empId', right_on = 'empId', how = 'left')
+
+    df = df[(df['bonus'] < 1000) | (df['bonus'].isna())]
+    return df[['name', 'bonus']]
